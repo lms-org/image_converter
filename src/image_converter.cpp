e94897c5e9fb40321b331615492ed4f98a177283
Philipp <philipp.schmette@tum.de>
2015-05-06 14:37:39 +0200

    adding filters-lazy commit

--- src/image_converter.cpp
+++ src/image_converter.cpp
@@ -3,23 +3,40 @@
 #include "image_converter.h"
 #include "lms/datamanager.h"
 #include "lms/imaging/converter.h"
+#include <lms/imaging/image_factory.h>
+
+void test(){
+
+}
 
 bool ImageConverter::initialize() {
     config = getConfig();
 
     outputFormat = lms::imaging::formatFromString(
                 config->get<std::string>("output_format"));
+    //TODO just for testing
+    //Not sure how to handle multiple filters, for example gauss first -> sobel values
+    //Maybe we should go for one filter per converter at first :)
+    std::vector<std::string> filters = config->getArray<std::string>("filter");
+    std::vector<void (*)(const lms::imaging::Image&,lms::imaging::Image&)> functions;
+    for(std::string filter : filters){
+        if(filter == "gauss"){
+            functions.push_back(lms::imaging::op::gauss);
+        }else if(filter == "sobelX"){
+            functions.push_back(lms::imaging::op::sobelX);
+        }else if(filter == "sobelY"){
+            functions.push_back(lms::imaging::op::sobelY);
+        }
+    }
 
     if(outputFormat == lms::imaging::Format::UNKNOWN) {
         logger.error("init") << "output_format is " << outputFormat;
         return false;
     }
-
     inputImagePtr = datamanager()
             ->readChannel<lms::imaging::Image>(this, "INPUT_IMAGE");
     outputImagePtr = datamanager()
             ->writeChannel<lms::imaging::Image>(this, "OUTPUT_IMAGE");
-
     return true;
 }
 
