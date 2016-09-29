f7d08e4fde6890b98f21114b7b91814d1934b1c5
phibedy <philipp.schmette@gmx.de>
2016-09-29 19:19:10 +0200

    moved to conan

---
+++ conanfile.py
@@ -0,0 +1,14 @@
+from conans import ConanFile, CMake
+
+class ImageConverterConan(ConanFile):
+    name = "lms_image_converter"
+    version = "1.0"
+    settings = "os", "compiler", "build_type", "arch"
+    exports = "include/*","src/*","CMakeList.txt","configs/*","README.md"
+    requires = "lms_imaging/1.0@lms/stable","lms_imaging_detection/1.0@lms/stable","lms/2.0@lms/stable"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self.settings)
+        self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
+        self.run("cmake --build . %s" % cmake.build_config)
